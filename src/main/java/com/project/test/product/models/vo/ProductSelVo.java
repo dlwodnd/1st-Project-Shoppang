58f8892cadb39e54de6c9a56528f0ab747c7f085
Greenstudy <leejaewoong300@gmail.com>
2023-12-26 18:01:35 +0900

    1차프로젝트 12-26

--- src/main/java/com/project/test/product/models/vo/ProductSelVo.java
+++ src/main/java/com/project/test/product/models/vo/ProductSelVo.java
@@ -6,22 +6,13 @@ import lombok.Data;
 @Data
 @Schema(name = "구매예정 상품 출력 정보")
 public class ProductSelVo {
-    @Schema(description = "구매예정 상품 pk값")
     private int productPk;
-    @Schema(description = "유저 pk값")
     private int userPk;
-    @Schema(description = "구매예정 상품명")
     private String productNm;
-    @Schema(description = "카테고리 pk값")
     private int categoryPk;
-    @Schema(description = "카테고리 이름")
     private String categoryNm;
-    @Schema(description = "구매예정 상품 메모")
     private String memo;
-    @Schema(description = "구매예정 상품 등록 날짜/시간")
     private String createdAt;
-    @Schema(description = "구매예정 상품 구매 여부. 0 : 구매예정, 1 : 구매확정, 2 : 구매확정 숨김")
     private int buyingCheck;
-    @Schema(description = "구매확정 날짜")
     private String buyingDate;
 }
